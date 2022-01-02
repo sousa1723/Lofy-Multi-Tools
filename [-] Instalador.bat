1e782f590aaceb4b59194b360686a783e13a9391
PolarLofy <75093985+PolarLofy@users.noreply.github.com>
2022-01-01 22:53:17 -0300

    Add files via upload

---
+++ [-] Instalador.bat
@@ -0,0 +1,28 @@
+@echo off
+color 4
+title Instalador
+echo.
+echo.
+echo.
+echo      Bem Vindo ao Instalador, Este processo pode demorar ate 5minutos dependendo
+echo      da sua maquina :P, isso e importante para que voce use o selfbot sem aver
+echo      problemas.
+echo.
+echo.
+echo.
+pause nul
+cls
+color 5
+call pip install colorama
+call pip install Fore
+call pip install requests
+call pip install threading
+call pip install discord
+call pip install asyncio
+call pip install Style
+call pip install Thread
+call npm i
+cls
+echo      Prontinho pode sair :
+echo.
+pause nul
