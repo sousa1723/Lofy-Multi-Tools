4c86b86eba4d7ede4dbd2066a97088886a68a4fb
PolarLofy <75093985+PolarLofy@users.noreply.github.com>
2022-02-25 19:25:53 -0300

    Update [-] Instalador.bat

--- [-] Instalador.bat
+++ [-] Instalador.bat
@@ -1,28 +1,59 @@
 @echo off
-color 4
-title Instalador
+color A
+title Lofy Multi Tools
 echo.
 echo.
 echo.
-echo      Bem Vindo ao Instalador, Este processo pode demorar ate 5minutos dependendo
-echo      da sua maquina :P, isso e importante para que voce use o selfbot sem aver
-echo      problemas.
+echo.
+echo      Bem vindo ao instalador do Lofy Multi Tools
+echo      Voce esta prestes a instalar todos os requisitos
+echo      Em caso de duvida entre no grupo do discord.
+echo      Este processo pode demorar dependendo de sua maquina
+echo.
+echo.
+echo.
 echo.
 echo.
 echo.
 pause nul
 cls
 color 5
-call pip install colorama
-call pip install Fore
-call pip install requests
-call pip install threading
-call pip install discord
-call pip install asyncio
-call pip install Style
-call pip install Thread
-call npm i
+python -m pip install --upgrade pip
 cls
-echo      Prontinho pode sair :
+pip uninstall -y colorama
+pip uninstall -y Fore
+pip uninstall -y requests
+pip uninstall -y threading
+pip uninstall -y discord
+pip uninstall -y asyncio
+pip uninstall -y Style
+pip uninstall -y Thread
+pip uninstall -y dhooks
+cls
+pip install colorama
+pip install Fore
+pip install requests
+pip install threading
+pip install discord
+pip install asyncio
+pip install Style
+pip install Thread
+pip install dhooks
+cls
+npm i
+cls
+color 4
+echo.
+echo.
+echo.
+echo.
+echo      Instalacao concluida pode fechar o instalador.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
 echo.
 pause nul
